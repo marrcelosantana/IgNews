761b298202b17797324ee4af0efc933fbaa14bf8
marrcelosantana <marcelosantana@alu.ufc.br>
2021-05-16 14:54:45 -0300

    BD 👾

--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
+import { query } from 'faunadb';
+
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
-import { query } from 'faunadb';
 
 import { fauna } from '../../../services/fauna';
 
@@ -12,43 +13,37 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
-  jwt: {
-    signingKey: process.env.SIGNING_KEY,              // -> Chave para tirar erro do Auth.
-  },
-  callbacks: {
-    async signIn(user, account, profile) {
+  callbacks:{
+    async signIn(user, account, profile){
 
-      const { email } = user;
-
-      try{                                 
+      const { email } = user
+      try {
         await fauna.query(
-          query.If(                                   // -> IF/ELSE para verificar usuário Duplicado.
+          query.If(                               // -> IF/ELSE para verificar usuário Duplicado.
             query.Not(
               query.Exists(
-                query.Match(                         // -> Equivale ao WHERE do Banco de Dados.
+                query.Match(                       // -> Equivale ao WHERE do Banco de Dados.
                   query.Index('user_by_email'),
                   query.Casefold(user.email)
                 )
               )
             ),
-            query.Create(                          // -> Método pra fazer a inserção.
-              query.Collection('users'),          // -> Nome da tabela.
-              { data: { email } }                //-> Dados do usuário que queremos inserir.
-            ), //else
-            query.Get(                          // -> Equivale ao SELECT do Banco de Dados.
+            query.Create(                                // -> Método pra fazer a inserção.
+              query.Collection('users'),                 // -> Nome da tabela.
+              { data: {email}}                           //-> Dados do usuário que queremos inserir.
+            ), 
+            query.Get(                                     // -> Equivale ao SELECT do Banco de Dados.
               query.Match(
                 query.Index('user_by_email'),
-                query.Casefold(user.email)        // -> Deixa todas as letras do email em minúsculo.
+                query.Casefold(user.email)                   // -> Deixa todas as letras do email em minúsculo.
               )
             )
           )
         )
-        
-        return true;                         // -> Significa que o Login deu certo.
-
+        return true                                        // -> Significa que o Login deu certo.
       } catch {
-        return false;                      // -> Login não deu certo.
+        return false                                      // -> Login não deu certo.
       }
-    },
+    }
   }
 });
